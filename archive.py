13649236f33660a13307248a27756e4628f333f7
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-07-02 17:05:15 +0800

    fix

--- archive.py
+++ archive.py
@@ -2,5 +2,3 @@ import shutil as sh
 
 def archive(task_id : str):
     sh.make_archive(f'./uploads/{task_id}/archive', 'zip', f'./uploads/{task_id}/output')
-    
-archive('26b63b23-91fb-448e-90fb-18048748ba47')
