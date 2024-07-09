adfaf1122b9c736f91c1ab215adbd14a57e76beb
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-07-10 00:23:12 +0100

    is_valid_uuid

--- utils.py
+++ utils.py
@@ -8,3 +8,10 @@ UPLOAD_FOLDER = 'uploads'
 
 def uploads_folder(task_id):
     return os.path.join(UPLOAD_FOLDER, task_id)
+
+def is_valid_uuid(uuid_str):
+    try:
+        uuid_obj = uuid.UUID(uuid_str, version=4)
+    except ValueError:
+        return False
+    return str(uuid_obj) == uuid_str
